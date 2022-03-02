847b3a29fee74172b861a5092f01f63496773a4b
mamatch <eteil.djoumatchoua@gmail.com>
2022-03-02 14:49:49 +0100

    + add tests

--- tests/dag_utils_test.py
+++ tests/dag_utils_test.py
@@ -1,6 +1,19 @@
 import pytest
 from airflow_home.dags import dag_utils
+import os
 
 
-def test_file_was_downloaded():
-    assert True
+class Test_dag_utils:
+    def test_file_was_downloaded():
+        url = "https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv"
+        local_path_to_home = "test_folder"
+        destination_file_path = "result.csv"
+        final_destination = os.path.join(local_path_to_home, destination_file_path)
+        dag_utils.download_file(
+            url=url,
+            local_path_to_home=local_path_to_home,
+            destination_file_path=destination_file_path,
+        )
+        assert (
+            os.path.exists(final_destination) and os.path.getsize(final_destination) > 0
+        )
